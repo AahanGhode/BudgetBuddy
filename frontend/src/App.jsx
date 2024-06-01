28c9fa00596d89d85806f53114251f9400a23977
AahanGhode <aahanghode2020@gmail.com>
2024-05-31 21:10:10 -0400

    CRUD added in client

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,22 +10,32 @@ import { GET_AUTHENTICATED_USER } from "./graphql/queries/user.query";
 import { Toaster } from "react-hot-toast";
 
 function App() {
-  const { loading, data, error } = useQuery(GET_AUTHENTICATED_USER);
+  const { loading, data } = useQuery(GET_AUTHENTICATED_USER);
 
-  console.log("Loading:", loading); 
-  console.log("Authenticated user:", data);
-  console.log("Error:", error);
-
-  if(loading) return null;
+  if (loading) return null;
 
   return (
     <>
       {data?.authUser && <Header />}
       <Routes>
-        <Route path='/' element={data.authUser ? <HomePage /> : <Navigate to='/login' />} />
-				<Route path='/login' element={!data.authUser ? <LoginPage /> : <Navigate to='/' />} />
-				<Route path='/signup' element={!data.authUser ? <SignUpPage /> : <Navigate to='/' />} />
-        <Route path="/transaction/:id" element={data.authUser ? <TransactionPage /> : <Navigate to='/login' />} />
+        <Route
+          path="/"
+          element={data.authUser ? <HomePage /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/login"
+          element={!data.authUser ? <LoginPage /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/signup"
+          element={!data.authUser ? <SignUpPage /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/transaction/:id"
+          element={
+            data.authUser ? <TransactionPage /> : <Navigate to="/login" />
+          }
+        />
         <Route path="*" element={<NotFound />} />
       </Routes>
       <Toaster />
